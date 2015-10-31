ba495a35e9f48f41a327957228ae38c7bf006081
Nathan Wilcox <nejucomo@gmail.com>
2015-10-31 13:26:09 -0700

    Collapse build/install cmds into a single command and allow arg lists for single commands (to allow line-wrapping in python).

--- build-docker.py
+++ build-docker.py
@@ -54,10 +54,9 @@ def main(args = sys.argv[1:]):
             w('ENV PREFIX={}', stowdir)
 
             w('WORKDIR {}', srcname)
-            for cmd in depsrc.buildcmds:
-                w('RUN {}', cmd)
-
-            for cmd in depsrc.installcmds:
+            for cmd in depsrc.buildinstcmds:
+                if type(cmd) is list:
+                    cmd = ' '.join(cmd)
                 w('RUN {}', cmd)
 
             w('RUN cd /usr/local/stow ; stow {}', depsrc.name)
@@ -74,26 +73,29 @@ def main(args = sys.argv[1:]):
                DOCKER_CTX])
 
 
-DEBS = [
+DEBS_PREFETCH = [
     'apt-utils',
+    'wget',
+    ]
+
+DEBS_BUILD = [
     'build-essential',
     'git-core',
     'stow',
-    'wget',
     'unzip',
+    'm4',
     ]
 
 
 class DepSrc (object):
-    def __init__(self, name, sha256, ext, urlbase, buildcmds, installcmds):
+    def __init__(self, name, sha256, ext, urlbase, buildinstcmds):
         self.name = name
         self.ext = ext
         self.sha256 = sha256
         self.dlname = '{name}{ext}'.format(**locals())
         self._url = '{urlbase}{name}{ext}'.format(**locals())
         self._wgetxtra = []
-        self.buildcmds = buildcmds
-        self.installcmds = installcmds
+        self.buildinstcmds = buildinstcmds
 
     @property
     def fetch_command(self):
@@ -104,15 +106,14 @@ class DepSrc (object):
 
 
 class GithubDepSrc (DepSrc):
-    def __init__(self, name, sha256, user, commit, buildcmds, installcmds):
+    def __init__(self, name, sha256, user, commit, buildinstcmds):
         self.name = name
         ext = self.ext = '.tar.gz'
         self.dlname = '{name}-{commit}{ext}'.format(**locals())
         self.sha256 = sha256
         self._url = 'https://github.com/{user}/{name}/archive/{commit}{ext}'.format(**locals())
         self._wgetxtra = ['-O', self.dlname]
-        self.buildcmds = buildcmds
-        self.installcmds = installcmds
+        self.buildinstcmds = buildinstcmds
 
 
 DEPSRCS = [
@@ -121,56 +122,76 @@ DEPSRCS = [
         sha256='7f8e9a804b9c6d07164cf754207be838ece1219425d64e28cfa3e70d5c759aaf',
         ext='.tar.bz2',
         urlbase='https://gmplib.org/download/gmp/',
-        buildcmds=[],
-        installcmds=[],
+        buildinstcmds=[
+            ['./configure', '--prefix=$PREFIX',
+             '--enable-cxx', '--disable-shared',
+            ],
+            "make CPPFLAGS='-fPIC'",
+            'make install',
+        ],
     ),
     DepSrc(
         name='boost_1_57_0',
         sha256='910c8c022a33ccec7f088bd65d4f14b466588dda94ba2124e78b8c57db264967',
         ext='.tar.bz2',
         urlbase='http://sourceforge.net/projects/boost/files/boost/1.57.0/',
-        buildcmds=[],
-        installcmds=[],
+        buildinstcmds=[
+            ['./bootstrap.sh', '--without-icu',
+             '--with-libraries={}'.format(
+                 ','.join([
+                     'chrono', 'filesystem', 'program_options',
+                     'system', 'thread', 'test',
+                 ])),
+            ],
+            #['./b2', '-d2', '-j2', '-d1',
+            # '--prefix=$PREFIX',
+            # '--layout=tagged',
+            # '--build-type=complete',
+            # 'stage'],
+            ['./b2', '-d0', '-j4',
+             '--prefix=$PREFIX',
+             '--layout=tagged',
+             '--build-type=complete',
+             'install'],
+        ],
     ),
     DepSrc(
         name='openssl-1.0.1k',
         sha256='8f9faeaebad088e772f4ef5e38252d472be4d878c6b3a2718c10a4fcebe7a41c',
         ext='.tar.gz',
         urlbase='https://www.openssl.org/source/',
-        buildcmds=[],
-        installcmds=[],
+        buildinstcmds=[],
     ),
     DepSrc(
         name='cryptopp562',
         sha256='5cbfd2fcb4a6b3aab35902e2e0f3b59d9171fee12b3fc2b363e1801dfec53574',
         ext='.zip',
         urlbase='http://www.cryptopp.com/',
-        buildcmds=["make static CXXFLAGS='-DNDEBUG -g -O2 -fPIC'"],
-        installcmds=["make install PREFIX=${PREFIX}"],
+        buildinstcmds=[
+            "make static CXXFLAGS='-DNDEBUG -g -O2 -fPIC'",
+            'make install PREFIX=${PREFIX}',
+        ],
     ),
     GithubDepSrc(
         name='xbyak',
         sha256='467a9037c29bc417840177f3ff5d76910d3f688f2f216dd86ced4a7ac837bfb0',
         user='herumi',
         commit='62fd6d022acd83209e2a5af8ec359a3a1bed3a50',
-        buildcmds=[],
-        installcmds=[],
+        buildinstcmds=[],
     ),
     GithubDepSrc(
         name='ate-pairing',
         sha256='37c05b4a60653b912a0130d77ac816620890d65a51dd9629ed65c15b54c2d8e0',
         user='herumi',
         commit='dd7889f2881e66f87165fcd180a03cf659bcb073',
-        buildcmds=[],
-        installcmds=[],
+        buildinstcmds=[],
     ),
     GithubDepSrc(
         name='libsnark',
         sha256='b5ec84a836d0d305407d5f39c8176bae2bb448abe802a8d11ba0f88f17e6d358',
         user='scipr-lab',
         commit='69f312f149cc4bd8def8e2fed26a7941ff41251d',
-        buildcmds=[],
-        installcmds=[],
+        buildinstcmds=[],
     ),
 ]
 
