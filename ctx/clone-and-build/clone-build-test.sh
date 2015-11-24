7909476fd21bd7f16388fd6eed17f602b738ee9e
Nathan Wilcox <nejucomo@gmail.com>
2015-11-24 09:17:57 -0800

    Switch CXXFLAGS based on the zerocashd libzerocask.mk package spec; update make command to follow; call full-test-suite.sh.

--- ctx/clone-and-build/clone-build-test.sh
+++ ctx/clone-and-build/clone-build-test.sh
@@ -7,8 +7,15 @@ ls -l /usr/local/include
 git clone 'https://github.com/Electric-Coin-Company/libzerocash'
 cd ./libzerocash
 
-make CXXFLAGS='-c -MMD -g -Wall -Wextra -Werror -Wfatal-errors -Wno-unused-parameter -std=c++11 -fPIC -Wno-unused-variable -DUSE_ASM -DCURVE_ALT_BN128 -I/usr/local/include/libsnark -I.'
+CXXFLAGS='-I. -I/usr/local/include/libsnark -DCURVE_ALT_BN128 -DBOOST_SPIRIT_THREADSAFE -DHAVE_BUILD_INFO -D__STDC_FORMAT_MACROS -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -std=c++11 -pipe -O2 -O0 -g -Wstack-protector -fstack-protector-all -fPIE -fvisibility=hidden -fPIC'
 
-./tests/merkleTest
-./tests/zerocashTest
+make all \
+     DEPINST='/usr/local/' \
+     CXXFLAGS="$CXXFLAGS" \
+     STATIC=1 \
+     MINDEPS=1 \
+     USE_MT=1 \
+     LINK_RT=1
+
+./tests/full-test-suite.sh
 
